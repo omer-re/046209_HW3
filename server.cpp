a472bc92b569040f17153bec159f44fe7afb3b1e
omer-re <1EkrV5mY>
2020-06-15 21:39:41 +0300

    day 2 for hw3

--- server.cpp
+++ server.cpp
@@ -37,15 +37,64 @@ typedef struct data_struct {
 
 
 //  function headers
-void recieveData(int socketfd, struct socketaddr_in *client_addr, socklen_t client_addr_length, FILE *fptr);
+void recieveData(int socketfd, struct sockaddr_in *client_addr, socklen_t client_addr_length, FILE *fptr);
 
 void WRQ_parser(char *buffer, wrq_struct *wrq);
 
-void ACK_response(int socketfd, int ack_num, struct socketaddr_in *client_addr, socklen_t client_addr_length);
+void ACK_response(int socketfd, int ack_num, struct sockaddr_in *client_addr, socklen_t client_addr_length);
 
 
 int main(int argc, char **argv) {
 
+    // verify arg num
+    if (argc < 2) {
+        printf("Expecting more arguments\n");  //TODO: check if there's a specific error message defined. couldn't find any.
+        fflush(stdout);
+        exit(1);
+    }
+
+    // establish new socket
+    int socketfd = socket(AF_INET, SOCKET_DGRAM, 0);
+    int num_bytes_recv = 0;
+    // sockaddr_in is a netinet lib struct
+    /**
+     * struct sockaddr_in {
+    short            sin_family;   // e.g. AF_INET
+    unsigned short   sin_port;     // e.g. htons(3490)
+    struct in_addr   sin_addr;     // see struct in_addr, below
+    char             sin_zero[8];  // zero this if you want to
+    };
+
+    struct in_addr {
+        unsigned long s_addr;  // load with inet_aton()
+    };
+     */
+    struct sockaddr_in my_addr, client_addr = {0};
+    wrq_struct Wrq = {0};
+    socklen_t client_addr_len = sizeof(client_addr);
+    char Wrq_buffer[MAX_WRQ];
+
+    // initialize my_addr
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(atoi(argv[1]));
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+    if (socketfd == -1)  // case failure
+    {
+        perror("TTFTP_ERROR: ");
+        fflush(stdout);
+        exit(1);
+    }
+
+    // binding socket to the requested port
+    if (bind(socketfd, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1) // case bind or listen failed
+    {
+        close(socketfd);
+        perror("TTFTP_ERROR: ");  // as defined for system call failure
+        fflush(stdout);
+        exit(1);
+    }
 //  given skeleton
 
     const int WAIT_FOR_PACKET_TIMEOUT = 3;
@@ -109,9 +158,26 @@ void WRQ_parser(char *buffer, wrq_struct *wrq) {
 }
 
 
+void recieveData(int socketfd, struct sockaddr_in *client_addr, socklen_t client_addr_length, FILE *fptr) {}
+
+/**responding by sending ACK
+ * sending an ACK with the proper ack_num to the client-side.
+ * function ends only after it was able to send the ACK successfully (otherwise it'll keep trying).
+ * @param ack_num - packet num we acknowledge
+ * @param socketfd, client, client_addr_length - are network parameters
+ */
+void ACK_response(int socketfd, int ack_num, struct sockaddr_in *client, socklen_t client_addr_length) {
+    ack_struct ack = {htons(4), htons(ack_num)}; // create an ack for sending
+    int bytes_sent = 0;
+    do { // keep trying to send the ack, until it succeeds
+        bytes_sent = sendto(socketfd, &ack, sizeof(ack), 0,
+                            (struct sockaddr_in *) client_addr, client_addr_length);
+        if (bytes_sent < 0) // sending of ack has falied
+            perror("TTFTP_ERROR: ");
+    } while (bytes_sent != sizeof(ack));
+    // success!! you are a lucky person
+    printf("OUT:ACK, %d\n", ack_num);
+    fflush(stdout);
+
+
 }
-
-void recieveData(int socketfd, struct socketaddr_in *client_addr, socklen_t client_addr_length, FILE *fptr) {}
-
-
-void ACK_response(int socketfd, int ack_num, struct socketaddr_in *client, socklen_t client_addr_length) {}
