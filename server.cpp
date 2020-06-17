86b789bb8b683620f8b0d3be047822f3a42a6dfa
omer-re <1EkrV5mY>
2020-06-17 14:05:44 +0300

    day 3 for hw3
    Basically done, but unchecked

--- server.cpp
+++ server.cpp
@@ -47,15 +47,16 @@ void ACK_response(int socketfd, int ack_num, struct sockaddr_in *client_addr, so
 int main(int argc, char **argv) {
 
     // verify arg num
-    if (argc < 2) {
-        printf("Expecting more arguments\n");  //TODO: check if there's a specific error message defined. couldn't find any.
+    if (argc < 2)
+    {
+        printf("ERROR: Expecting more arguments\n");  //Lior said we can phrase whatever message we want
         fflush(stdout);
         exit(1);
     }
 
     // establish new socket
     int socketfd = socket(AF_INET, SOCKET_DGRAM, 0);
-    int num_bytes_recv = 0;
+    int recvMsgSize = 0;
     // sockaddr_in is a netinet lib struct
     /**
      * struct sockaddr_in {
@@ -95,48 +96,61 @@ int main(int argc, char **argv) {
         fflush(stdout);
         exit(1);
     }
-//  given skeleton
+/** wait for requests,
+ * validate requests,
+ * respond accordingly (ACK) and process
+ */
+    while (1)
+    {
+        //  clear buffers to ensure valid data
+        memset(&Wrq_buffer, 0, MAX_WRQ);
+        memset(&Wrq, 0, sizeof(wrq_struct));
 
-    const int WAIT_FOR_PACKET_TIMEOUT = 3;
-    const int NUMBER_OF_FAILURES = 7;
-    do {
-        do {
-            do {
-                // TODO: Wait WAIT_FOR_PACKET_TIMEOUT to see if something appears
-                // for us at the socket (we are waiting for DATA)
-                if ()// TODO: if there was something at the socket and
-                    // we are here not because of a timeout
-                {
-                    // TODO: Read the DATA packet from the socket (at
-                    // least we hope this is a DATA packet)
-                }
-                if (...) // TODO: Time out expired while waiting for data
-                // to appear at the socket
-                {
-                    //TODO: Send another ACK for the last packet
-                    timeoutExpiredCount++;
-                }
-                if (timeoutExpiredCount >= NUMBER_OF_FAILURES) {
-                    // FATAL ERROR BAIL OUT
-                }
-            } while (...) // TODO: Continue while some socket was ready
-            // but recvfrom somehow failed to read the data
-            if (...) // TODO: We got something else but DATA
-            {
-                // FATAL ERROR BAIL OUT
-            }
-            if (...) // TODO: The incoming block number is not what we have
-            // expected, i.e. this is a DATA pkt but the block number
-            // in DATA was wrong (not last ACK’s block number + 1)
-            {
-                // FATAL ERROR BAIL OUT
-            }
-        } while (FALSE);
-        timeoutExpiredCount = 0;
-        lastWriteSize = fwrite(...); // write next bulk of data
-        // TODO: send ACK packet to the client
+        /* Block until receive message from a client. Networking tutorial P.58*/
+        if (recvMsgSize =
+                    recvfrom(socketfd, Wrq_buffer, MAX_WRQ, 0, (struct sockaddr *) &client_addr_len, &client_addr_len) <
+                    0)
+        {
+            perror("TTFTP_ERROR: ");
+            fflush(stdout);
+            continue;
+        }
+        //  struct the WRQ
+        WRQ_parser(Wrq_buffer, &Wrq);
 
-    } while (...); // Have blocks left to be read from client (not end of transmission)
+        // validate wrq
+        if (Wrq.opcode != 2 || strcmp(wrq.trans_mode, "octet"))
+        {  //  invalid wrq
+            continue;   // todo: check if there are defined error messages or actions for this case
+        }
+
+        // the packet is a valid WRQ
+        printf("IN:WRQ, %s, %s\nWhere %s and %s are values of appropriate fields in the packet.\n", wrq.filename,
+               wrq.trans_mode, wrq.filename, wrq.trans_mode);
+        fflush(stdout);
+
+        FILE *fptr = fopen(wrq.filename, "w");
+        if (fptr == NULL)
+        {
+            //  generic system-call failure message
+            perror("TTFTP_ERROR: ");
+            fflush(stdout);
+            continue;
+        }
+
+        //  respond WRQ with ACK
+        ACK_response(socketfd, % client_addr, client_addr_len);
+        recieveData(socketfd, &client_addr, client_addr_len, fptr);
+
+        if (fclose(fptr) != 0)
+        {
+            //  generic system-call failure message
+            perror("TTFTP_ERROR: ");
+            fflush(stdout);
+        }
+    }
+
+}
 
 
 }
@@ -157,8 +171,146 @@ void WRQ_parser(char *buffer, wrq_struct *wrq) {
 
 }
 
+/**
+ * Once we start communicate with the client and processed the WRQ
+ * writing the input data to fptr
+ */
+void recieveData(int socketfd, struct sockaddr_in *client_addr, socklen_t client_addr_length, FILE *fptr) {
+    const int WAIT_FOR_PACKET_TIMEOUT = 3;
+    const int NUMBER_OF_FAILURES = 7;
+    fd_set fd_recieve;
+    struct timeval timeout;  // as recommended on pdf
+    int num_of_ready = 0;
+    int num_of_timeoutExpired = 0;
+    int ack_num = 0;
+    int recvMsgSize = 0
+    data_struct data;
 
-void recieveData(int socketfd, struct sockaddr_in *client_addr, socklen_t client_addr_length, FILE *fptr) {}
+    FD_ZERO(&fd_recieve); //  Initializes the file descriptor set fdset to have zero bits for all file descriptors.
+    FD_SET(socketfd, &fd_recieve);  //  Sets the bit for the file descriptor fd in the file descriptor set fdset.
+
+
+    //  given skeleton
+
+
+    do
+    {
+        memset(&data, 0, sizeof(data));  //  recieve new input data packets
+        do
+        {
+            do
+            {
+
+                /*************************************************************/
+                /* Initialize the timeval struct to 3 minutes.  If no        */
+                /* activity after 3 minutes this program will end.           */
+                /*************************************************************/
+                timeout.tv_sec = WAIT_FOR_PACKET_TIMEOUT;
+                timeout.tv_usec = 0;
+
+                /**********************************************************/
+                /* Call select() and wait 3 minutes for it to complete.   */
+                /**********************************************************/
+                num_of_ready = select(socketfd + 1, &fdrecv, NULL, NULL, &timeout);
+
+                /**********************************************************/
+                /* Check to see if the 3 minute time out expired.         */
+                /**********************************************************/
+                if (num_of_ready == 0)
+                {  //  there was a data at the socket
+                    //  resend last ACK
+                    printf("FLOWERROR: timeout expired. packet did not arrive on time\n");
+                    fflush(stdout);
+                    num_of_timeoutExpired++;
+                    ACK_response(socketfd, ack_num, client_addr, client_addr_length);
+                }
+
+                /**********************************************************/
+                /* Check to see if the select call succeeded.
+                 * timeout wasn't a problem*/
+                /**********************************************************/
+                if (num_of_ready > 0)
+                {
+                    //  process the data packet from the socket
+                    recvMsgSize = recvfrom(socketfd, DATA_PACKET_SIZE, 0, (struct sockaddr *) &client_addr_len,
+                                           &client_addr_len);
+                    if (recvMsgSize < 0)
+                    {
+                        // generic system-call failure message
+                        perror("TTFTP_ERROR: ");
+                        fflush(stdout);
+                        continue;
+                    }
+                    else
+                    { //  convert to endian
+                        data.opcode = nthos(data.opcode);
+                        data.block_num = ntohs(data.block_num);
+                        break; // packet been received, go process it.
+                    }
+                }
+
+                if (num_of_timeoutExpired >= NUMBER_OF_FAILURES)  // reached maximum timeouts
+                {// FATAL ERROR BAIL OUT
+                    printf("FLOWERROR: exceeded number of allowed timeouts, bailing out...\n");
+                    printf("RECVFAIL\n");
+                    fflush(stdout);
+                    return;
+                }
+            } while (1); // keep running as long sockets are ready, but recvfrom failed.
+
+            if (data.opcode != 3)  // this isn't data
+            {
+                // FATAL ERROR BAIL OUT
+                printf("RECVFAIL\n");
+                fflush(stdout);
+                return;
+            }
+
+            if (data.block_num != ack_num + 1)
+            {  //  that is not the block we expected, but this is a valid data packet
+
+                // FATAL ERROR BAIL OUT
+                printf("FLOWERROR: block number does not agree with expected\n");
+                printf("RECVFAIL\n");
+                fflush(stdout);
+                return;
+            }
+        } while (false) // TODO: wtf is this condition?!?!
+
+        //  this is where we know that our packet is valid and matches expectations
+        printf("IN:DATA, %d, %d\n", data.block_num, bytes_recv);
+        fflush(stdout);
+        num_of_timeoutExpired = 0;
+
+        //  print all but header
+        printf("WRITING: %d\n", recvMsgSize - 4);
+        fflush(stdout);
+        res = fwrite(data.data, 1, recvMsgSize - 4, fptr);
+
+        if (fwrite(data.data, 1, recvMsgSize - 4, fptr) != recvMsgSize - 4)
+        {    // case it failed to write the data to the file
+            // FATAL ERROR BAIL OUT
+            perror("TTFTP_ERROR: ");
+            printf("RECVFAIL\n");
+            fflush(stdout);
+            return;
+        }
+
+        else
+        {
+            // case writing is successful
+            // respond the client with an ACK
+            ack_num++;
+            ACK_response(socketfd, ack_num, client_addr, client_addr_length);
+
+        }
+        while (recvMsgSize >= DATA_PACKET_SIZE);  // means we still have left blocks from the client to read
+
+        //  all data was transmitted
+        printf("RECVOK\n");
+        fflush(stdout);
+    }
+}
 
 /**responding by sending ACK
  * sending an ACK with the proper ack_num to the client-side.
@@ -169,7 +321,8 @@ void recieveData(int socketfd, struct sockaddr_in *client_addr, socklen_t client
 void ACK_response(int socketfd, int ack_num, struct sockaddr_in *client, socklen_t client_addr_length) {
     ack_struct ack = {htons(4), htons(ack_num)}; // create an ack for sending
     int bytes_sent = 0;
-    do { // keep trying to send the ack, until it succeeds
+    do
+    { // keep trying to send the ack, until it succeeds
         bytes_sent = sendto(socketfd, &ack, sizeof(ack), 0,
                             (struct sockaddr_in *) client_addr, client_addr_length);
         if (bytes_sent < 0) // sending of ack has falied
